ef762ad435bd497acfb167768eff651ef613c6e3
marcelo.r.oliveira <mclricardo@gmail.com>
2021-03-21 00:30:00 -0300

    ejs rendering

--- pdf-app/routes/index.js
+++ pdf-app/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let rawdata = fs.readFileSync('data/data.json');
+  let papers = JSON.parse(rawdata);
+
+  res.render('index', { title: 'Embedding PDF', papers: papers });
 });
 
 module.exports = router;
--- pdf-app/routes/papers.js
+++ pdf-app/routes/papers.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const fs = require('fs');
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  let rawdata = fs.readFileSync('data/data.json');
+  let papers = JSON.parse(rawdata);
+  res.send(papers);
 });
 
 module.exports = router;
